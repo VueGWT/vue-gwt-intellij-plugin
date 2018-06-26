62d544e5496c83582812ce0ab1c2da00c9a74cf6
Justin Trentesaux <justin.trentesaux@axellience.com>
2018-06-26 09:58:06 +0200

    Add marker to navigate from the HTML template to java file

---
+++ src/main/java/com/axellience/vuegwtplugin/markers/TemplateToJavaMarker.java
@@ -0,0 +1,50 @@
+package com.axellience.vuegwtplugin.markers;
+
+import com.axellience.vuegwtplugin.VueGWTBundle;
+import com.axellience.vuegwtplugin.VueGWTIcons;
+import com.axellience.vuegwtplugin.util.VueGWTPluginUtil;
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
+import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.html.HtmlTag;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+
+import static com.axellience.vuegwtplugin.util.VueGWTPluginUtil.findJavaFromTemplate;
+
+public class TemplateToJavaMarker extends RelatedItemLineMarkerProvider {
+
+  @Override
+  protected void collectNavigationMarkers(@NotNull PsiElement element,
+      @NotNull Collection<? super RelatedItemLineMarkerInfo> result) {
+
+    if (!(element instanceof HtmlTag)) {
+      return;
+    }
+
+    HtmlTag tag = (HtmlTag) element;
+    if (tag.getParentTag() != null || isVueGwtNamespace(tag)) {
+      return;
+    }
+
+    PsiFile templateFile = tag.getContainingFile();
+    findJavaFromTemplate(templateFile)
+        .ifPresent(javaFile -> addMarker(element, result, javaFile));
+  }
+
+  private boolean isVueGwtNamespace(HtmlTag tag) {
+    return VueGWTPluginUtil.VUE_GWT_NAMESPACE.equals(tag.getNamespace());
+  }
+
+  private void addMarker(PsiElement element,
+      Collection<? super RelatedItemLineMarkerInfo> result,
+      PsiFile javaFile) {
+    result.add(NavigationGutterIconBuilder.create(VueGWTIcons.VUE)
+        .setTarget(javaFile)
+        .setTooltipText(VueGWTBundle.message("navigatemarker.tooltip"))
+        .createLineMarkerInfo(element));
+  }
+}
--- src/main/java/com/axellience/vuegwtplugin/util/VueGWTPluginUtil.java
+++ src/main/java/com/axellience/vuegwtplugin/util/VueGWTPluginUtil.java
@@ -1,7 +1,5 @@
 package com.axellience.vuegwtplugin.util;
 
-import static com.intellij.psi.impl.PsiImplUtil.findAttributeValue;
-
 import com.google.common.base.CaseFormat;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiAnnotation;
@@ -9,12 +7,18 @@ import com.intellij.psi.PsiAnnotationMemberValue;
 import com.intellij.psi.PsiClassType;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiFile;
+
 import java.util.Arrays;
 import java.util.Optional;
 
+import static com.intellij.psi.impl.PsiImplUtil.findAttributeValue;
+
 public class VueGWTPluginUtil {
+
   public static final String COMPONENT_QUALIFIED_NAME =
       "com.axellience.vuegwt.core.annotations.component.Component";
+
+  public static final String VUE_GWT_NAMESPACE = "http://axellience.com/vue-gwt";
 
   private VueGWTPluginUtil() {
 
@@ -27,39 +31,59 @@ public class VueGWTPluginUtil {
     }
 
     String templateName = getTemplateNameFrom(javaFile);
-    return Arrays.stream(parentDirectory.getFiles())
-        .filter(file -> templateName.equals(file.getName()))
-        .findFirst();
+    return findFileWithName(parentDirectory, templateName);
   }
 
   public static String getTemplateNameFrom(PsiFile javaFile) {
     return javaFile.getName().replaceAll("(.*)\\.java", "$1.html");
   }
 
-  public static Optional<VirtualFile> getJavaFileForTemplate(VirtualFile templateFile)
-  {
-    if (templateFile == null || !"html".equals(templateFile.getExtension()))
+  public static Optional<PsiFile> findJavaFromTemplate(PsiFile templateFile) {
+    PsiDirectory parentDirectory = templateFile.getContainingDirectory();
+    if (parentDirectory == null) {
       return Optional.empty();
+    }
+
+    String javaName = getJavaNameFromTemplate(templateFile);
+    return findFileWithName(parentDirectory, javaName);
+  }
+
+  public static String getJavaNameFromTemplate(PsiFile templateFile) {
+    return templateFile.getName().replaceAll("(.*)\\.html", "$1.java");
+  }
+
+  private static Optional<PsiFile> findFileWithName(PsiDirectory directory, String name) {
+    return Arrays.stream(directory.getFiles())
+        .filter(file -> name.equals(file.getName()))
+        .findFirst();
+  }
+
+  // ------ OLD ------------------------------------------------------------------------------------
+
+  public static Optional<VirtualFile> getJavaFileForTemplate(VirtualFile templateFile) {
+    if (templateFile == null || !"html".equals(templateFile.getExtension())) {
+      return Optional.empty();
+    }
 
     String javaClassFileName = templateFile.getNameWithoutExtension() + ".java";
     VirtualFile parent = templateFile.getParent();
-    if (parent == null)
+    if (parent == null) {
       return Optional.empty();
+    }
 
     return Optional.ofNullable(parent.findChild(javaClassFileName));
   }
 
-  public static String componentToTagName(PsiClassType componentClass)
-  {
+  public static String componentToTagName(PsiClassType componentClass) {
     PsiAnnotation componentAnnotation =
         componentClass.findAnnotation(COMPONENT_QUALIFIED_NAME);
 
-    if (componentAnnotation != null)
-    {
+    if (componentAnnotation != null) {
       PsiAnnotationMemberValue name = findAttributeValue(componentAnnotation, "name");
 
-      if (name != null && !"".equals(name.getText()))
+      if (name != null && !"".equals(name.getText())) {
         return name.getText();
+      }
     }
 
     String componentClassName = componentClass.getName().replaceAll("Component$", "");
